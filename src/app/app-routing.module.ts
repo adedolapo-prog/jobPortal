086f6715b12bda45d2a7c7154fbbfff0242fe23e
Adedolapo Adegoke <shayne_huey@Adedolapos-MacBook-Pro.local>
2024-06-20 17:38:37 -0400

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
-import { LoginComponent } from './login/login.component';
+import { SignIn } from './login/signIn.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: SignIn },
 ];
 
 @NgModule({
